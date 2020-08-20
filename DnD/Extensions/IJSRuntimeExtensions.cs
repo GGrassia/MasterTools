bc8554b8736f589f9f56c97ae437401c5d06623b
Giulio Grassia <giuliograssia@gg.com>
2020-08-20 08:28:57 +0200

    Add project files.

---
+++ DnD/Extensions/IJSRuntimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace DnD.Extensions
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static void Alert(this IJSRuntime js, string message)
+        {
+            js.InvokeVoidAsync("alert", new object[] { message });
+        }
+    }
+}
---
+++ DnD/Models/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnD.Models
+{
+    public class Item
+    {
+        //If the item is a weapon, piece of armour, a kit or stuff you've found
+        public string Type { get; set; }
+        //How many of this item you have in the inventory, specially useful for potions
+        public int Number { get; set; }
+        //Is the item in question magic or not
+        public bool Magic { get; set; }
+        //The name of the item
+        public string Name { get; set; }
+        //An indicative sum you would make from selling the item, or the countervalue for a component (IE the diamond dust for stoneskin)
+        public int Value { get; set; }
+        //Description of the item, it's function or properties, if necessary
+        public string Description { get; set; }
+        //Name of the character currently keeping or owning the item
+        public string Owner { get; set; }
+
+    }
+}

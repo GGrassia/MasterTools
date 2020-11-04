f519a692e8d357987f44b1ac82a96c2293b4e3a4
Giulio Grassia <giuliograssia@gg.com>
2020-11-04 16:55:52 +0100

    Co-authored-by: scarfacetheduke@outlook.it <scarfacetheduke@outlook.it>

---
+++ DnD/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DnD
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=blogging.db");
+    }
+}
